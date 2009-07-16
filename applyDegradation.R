ec6572a0036efbe6651ea130fdef85f552fee4f4
Yeison Rodriguez <jaynano@gmail.com>
2009-07-16 01:36:39 -0400

    git does not like emacs

--- applyDegradation.R
+++ applyDegradation.R
@@ -1,4 +1,8 @@
 applyDegradation <- function(bm){
-	diag(bm) = rnorm(length(bm[1,]), -0.005, 0.002)
+<<<<<<< HEAD:applyDegradation.R
+	diag(bm) = rnorm(length(bm[1,]), -0.0006, 0.0002)
+=======
+	diag(bm) = rnorm(length(bm[1,]), -0.0006, 0.002)
+>>>>>>> 7fa0628e5a0398be26c0e7c7ec0c41312ee3d232:applyDegradation.R
 	return(bm)
 }
