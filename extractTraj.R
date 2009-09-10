58cbdf91f87266ed97907e6b88fcd02a6e4dff70
Yeison Rodriguez <jaynano@gmail.com>
2009-09-10 12:44:37 -0400

    Added various new data point extraction functions.
    Amongst the new data point extraction functions, there is a function
    that simulates an affymetrix experiment.  The function adds certain
    amount of noise to each extraction points.  In this way we include
    experimental error into our model.

--- extractTraj.R
+++ extractTraj.R
@@ -1,9 +1,48 @@
-extractTraj <- function(synth, npoints) {
+# The function extractTraj will extract the given number of 
+# points of data, divided into even intervals, from the provided
+# syntheticDataSet.
+extractTraj <- function(syntheticDataSet, numberOfPoints) {
+	extractWithNoise(syntheticDataSet, numberOfPoints, 0)
+}
+
+# extractWithNoise adds the given amount of noise to the extracted
+# points.  This is to simulate something such as an assay, where
+# experimental error may yield some unwanted errors in the results. 
+extractWithNoise <- function(syntheticDataSet, numberOfPoints, noise){
+	synth = syntheticDataSet
+	npoints = numberOfPoints
 	rows = dim(synth)[1]
 	dt = dim(synth)[2]/npoints	
 	extracted = matrix(0, nr=rows, nc=npoints)
-	extracted[,1] = synth[,1]
+	extracted[ ,1] = synth[ ,1]
 	for (i in 2:npoints)
-		extracted[,i] = synth[ ,i*dt]
+		extracted[ ,i] = synth[ ,i*dt] + synth[ ,i*dt]*rnorm(1, 0, noise)
 	return(extracted)
 }
+
+# Abbreviated function name.
+ewn <- extractWithNoise
+
+
+extractWithMeanNoise <- function(syntheticDataSet, numberOfPoints){
+	synth = syntheticDataSet
+	npoints = numberOfPoints
+	rows = dim(synth)[1]
+	dt = dim(synth)[2]/npoints	
+	extracted = matrix(0, nr=rows, 	nc=npoints)
+	extracted[ ,1] = synth[ ,1]
+	for(i in 2:npoints){
+		for(j in 1:rows){
+			meanNoise = mean(synth[j, ])
+			stdv = sd(synth[j,])
+			if((temp = synth[j, i*dt] + rnorm(1, meanNoise, stdv)) > 0)
+				extracted[j, i] = temp
+			else
+				if(extracted[j, i] < 0)
+					extracted[j, i] = 0
+		}
+	}
+	return(extracted)
+}
+
+ewmn <- extractWithMeanNoise
